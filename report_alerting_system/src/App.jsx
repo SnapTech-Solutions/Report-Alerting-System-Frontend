20d33cff1ce41d192e42068f5cd2e865e3ff9df7
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2024-01-24 19:27:50 +0530

    Initial Commit
    Created react app and created relevant components.

---
+++ report_alerting_system/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HomePageComponent from './components/HomePageComponent'
+
+function App() {
+
+  return (
+    <>
+     <HomePageComponent />
+    </>
+  )
+}
+
+export default App
