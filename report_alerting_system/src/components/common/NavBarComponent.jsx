91a6224d83290ccd5450089d30e7a224ca1ce6a6
ShaniWasana <rukshaniwasana59@gmail.com>
2024-01-27 17:40:23 +0530

    completed Button Components

--- report_alerting_system/src/components/common/NavBarComponent.jsx
+++ report_alerting_system/src/components/common/NavBarComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../common/css/NavBarComponent.css'
 import LogOutComponent from './LogOutComponent'
+import ButtonComponents from './ButtonComponents'
 
 function NavBarComponent() {
   return (
@@ -12,23 +13,22 @@ function NavBarComponent() {
      
       <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
       
-      <li><a href="/report" class="nav-link pt-5" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
-            <img src='src/assets/internet_13895721.png' />
-          </a>
-        </li>
+      <li> <a href="/report" class="nav-link pt-5" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+           <ButtonComponents path="src/assets/internet_13895721.png"/>
+          </a> </li>
         <li>
           <a href="/report" class="nav-link pt-5" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
-            <img src='src/assets/people_13895698.png' />
+            <ButtonComponents path='src/assets/people_13895698.png' />
           </a>
         </li>
         <li>
           <a href="/chat" class="nav-link pt-5" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-          <img src='src/assets/monitor_13895562.png' /></a>
+          <ButtonComponents path='src/assets/monitor_13895562.png' /></a>
         </li>
         
         <li>
           <a href="#" class="nav-link pt-5" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
-          <img src='src/assets/settings_13895710.png' /> </a>
+          <ButtonComponents path='src/assets/settings_13895710.png' /> </a>
         </li>
         <li>
           <a href="#" class="nav-link py-4 mt-6" title="Customers" data-bs-toggle="tooltip" data-bs-placement="right">
--- report_alerting_system/src/components/common/NotificationBarComponent.jsx
+++ report_alerting_system/src/components/common/NotificationBarComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../common/css/NotificationBarComponent.css'
 import UserDetailsComponent from './UserDetailsComponent'
+import ButtonComponents from './ButtonComponents'
 
 function NotificationBarComponent() {
   return (
@@ -9,10 +10,10 @@ function NotificationBarComponent() {
 
       <ul class="nav justify-content-end bg-dark  " >
         <li class="nav-item mr-4">
-          <a class="nav-link active" href="#"><img src='src/assets/communication_13894348.png'/></a>
+          <a class="nav-link active" href="#"><ButtonComponents path='src/assets/communication_13894348.png'/></a>
         </li>
         <li class="nav-item mr-4">
-          <a class="nav-link" href="#"><img src='src/assets/bell_13912720.png' /></a>
+          <a class="nav-link" href="#"><ButtonComponents path='src/assets/bell_13912720.png' /></a>
         </li>
         <li class="nav-item mr-4 ">
          <UserDetailsComponent name="ward 1"/>
